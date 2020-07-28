32614bf90a91f8d3bfe07d41cb31959934fab401
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-07-28 22:07:09 +0200

    Initial get command structure

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
+use anyhow::Result;
 use log::debug;
+use structopt::StructOpt;
 
-fn main() {
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "je",
+    about = "Jcr Exchange - easy download and upload files to and from JCR"
+)]
+enum Opt {
+    /// Download server content to local file server
+    Get {
+        /// path to download
+        path: String,
+    },
+}
+
+fn main() -> Result<()> {
     pretty_env_logger::init();
-    debug!("Starting");
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    debug!("parsed opts: {:#?}", opt);
+    match opt {
+        Opt::Get { path } => get(path)?,
+    }
+    Ok(())
+}
+
+fn get<S: Into<String>>(path: S) -> Result<()> {
+    debug!("downloading path {}", path.into());
+    Ok(())
 }
