957ec9e8db4989c7fdef7c4bec5f2c54370b2b3f
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-08-02 19:45:03 +0200

    Add test of creating jcr_root dir

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
 use log::debug;
+use std::env;
+use std::fs::create_dir_all;
+use std::path::Path;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -26,6 +29,59 @@ fn main() -> Result<()> {
 }
 
 fn get<S: Into<String>>(path: S) -> Result<()> {
-    debug!("downloading path {}", path.into());
+    // mkdir -p "$tmpDir/jcr_root$filterDirname"
+    let path = path.into();
+    mk_jcr_root_dir(&path)?;
+    copy_files()?;
+    build_pkg()?;
+    upload_pkg()?;
+    install_pkg()?;
+    cleanup_tmp()?;
+    debug!("downloading path {}", path);
     Ok(())
+}
+
+fn mk_jcr_root_dir(path: &str) -> Result<()> {
+    let parts: Vec<&str> = path.split("jcr_root/").collect();
+    assert_eq!(parts.len(), 2);
+    let path = parts[1];
+    let tmp_dir = env::temp_dir();
+    let tmp_dir_path = tmp_dir.as_path().to_str().unwrap_or("/tmp");
+    let path = format!("{}/je/jcr_root/{}", tmp_dir_path, path);
+    create_dir_all(path)?;
+    Ok(())
+}
+
+fn copy_files() -> Result<()> {
+    Ok(())
+}
+
+fn build_pkg() -> Result<()> {
+    Ok(())
+}
+
+fn upload_pkg() -> Result<()> {
+    Ok(())
+}
+
+fn install_pkg() -> Result<()> {
+    Ok(())
+}
+
+fn cleanup_tmp() -> Result<()> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::mk_jcr_root_dir;
+    use anyhow::Result;
+    use std::path::Path;
+
+    #[test]
+    fn test_mk_jcr_root_dir() -> Result<()> {
+        mk_jcr_root_dir("/home/zbychu/test/jcr_root/some/dir")?;
+        assert_eq!(Path::new("/tmp/je/jcr_root").exists(), true);
+        Ok(())
+    }
 }
