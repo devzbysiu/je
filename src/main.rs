5120d3b2b098ab7a51fb225b5406f288458e574b
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-08-02 20:20:10 +0200

    Cover filter contents with tests

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() -> Result<()> {
 fn get<S: Into<String>>(path: S) -> Result<()> {
     // mkdir -p "$tmpDir/jcr_root$filterDirname"
     let path = path.into();
-    mk_jcr_root_dir()?;
+    mk_jcr_root_dir(&path)?;
     build_pkg()?;
     upload_pkg()?;
     install_pkg()?;
@@ -44,7 +44,7 @@ fn get<S: Into<String>>(path: S) -> Result<()> {
     Ok(())
 }
 
-fn mk_jcr_root_dir() -> Result<()> {
+fn mk_jcr_root_dir(path: &str) -> Result<()> {
     let tmp_dir = env::temp_dir();
     let tmp_dir_path = tmp_dir.as_path().to_str().unwrap_or("/tmp");
     let jcr_root_path = format!("{}/je/jcr_root", tmp_dir_path);
@@ -53,13 +53,20 @@ fn mk_jcr_root_dir() -> Result<()> {
     let vault_path = format!("{}/je/META-INF/vault", tmp_dir_path);
     create_dir_all(&vault_path)?;
 
+    let parts: Vec<&str> = path.split("jcr_root/").collect();
+    assert_eq!(parts.len(), 2);
+    let content_path = parts[1];
+
     let mut filter_file = File::create(format!("{}/filter.xml", vault_path))?;
     filter_file.write_all(
-        r#"<?xml version="1.0" encoding="UTF-8"?>
+        format!(
+            r#"<?xml version="1.0" encoding="UTF-8"?>
 <workspaceFilter version="1.0">
-    <filter root="$(to_xml $filter)"/>
+    <filter root="/{}"/>
 </workspaceFilter>
-        "#
+        "#,
+            content_path
+        )
         .as_bytes(),
     )?;
     Ok(())
@@ -97,16 +104,27 @@ fn cleanup_tmp() -> Result<()> {
 mod test {
     use super::mk_jcr_root_dir;
     use anyhow::Result;
+    use std::fs::read_to_string;
     use std::path::Path;
 
     #[test]
     fn test_mk_jcr_root_dir() -> Result<()> {
-        mk_jcr_root_dir()?;
+        mk_jcr_root_dir("/home/user/project/jcr_root/content/client")?;
         assert_eq!(Path::new("/tmp/je/jcr_root").exists(), true);
         assert_eq!(
             Path::new("/tmp/je/META-INF/vault/filter.xml").exists(),
             true
         );
+        let filter_contents = read_to_string("/tmp/je/META-INF/vault/filter.xml")?;
+        assert_eq!(
+            filter_contents,
+            r#"<?xml version="1.0" encoding="UTF-8"?>
+<workspaceFilter version="1.0">
+    <filter root="/content/client"/>
+</workspaceFilter>
+        "#
+        );
+
         Ok(())
     }
 }
