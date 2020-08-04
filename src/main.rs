6f789ce90db95fc2cd910cd5d39f5e77f6f611aa
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-08-04 13:44:39 +0200

    Move pkg related fns to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::pkg::{mk_pkg_dir, Pkg};
 use anyhow::Result;
 use base64::encode;
 use log::debug;
@@ -9,16 +10,16 @@ use std::fs::{create_dir_all, read_to_string, remove_dir_all, File, OpenOptions}
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
-use std::path::PathBuf;
 use std::thread;
 use std::time::Duration;
-use std::time::{SystemTime, UNIX_EPOCH};
 use structopt::StructOpt;
 use tempfile::TempDir;
 use walkdir::WalkDir;
 use zip::write::FileOptions;
 use zip::ZipArchive;
 use zip::ZipWriter;
+
+mod pkg;
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -32,33 +33,6 @@ enum Opt {
         path: String,
     },
     Init,
-}
-
-#[derive(Debug)]
-struct Pkg {
-    name: String,
-    version: String,
-    group: String,
-}
-
-impl Pkg {
-    fn path(&self) -> String {
-        format!("{}/{}-{}.zip", self.group, self.name, self.version)
-    }
-}
-
-impl Default for Pkg {
-    fn default() -> Self {
-        Self {
-            name: "je-pkg".into(),
-            version: SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .expect("failed to count secs from EPOCH")
-                .as_secs()
-                .to_string(),
-            group: "je".into(),
-        }
-    }
 }
 
 #[derive(Debug, Default, Serialize, Deserialize)]
@@ -123,88 +97,6 @@ fn get<S: Into<String>>(cfg: &Cfg, path: S) -> Result<()> {
     Ok(())
 }
 
-fn mk_pkg_dir(path: &str, pkg: &Pkg) -> Result<TempDir> {
-    debug!("creating pkg dir");
-    let tmp_dir = TempDir::new()?;
-    mk_jcr_root_dir(&tmp_dir)?;
-    mk_vault_dir(&tmp_dir)?;
-    write_filter_content(&tmp_dir, content_path(path))?;
-    write_properties_content(&tmp_dir, pkg)?;
-    Ok(tmp_dir)
-}
-
-fn mk_jcr_root_dir(tmp_dir: &TempDir) -> Result<()> {
-    let jcr_root_dir_path = tmp_dir.path().join("jcr_root");
-    debug!("creating jcr_root dir: {}", jcr_root_dir_path.display());
-    create_dir_all(jcr_root_dir_path)?;
-    Ok(())
-}
-
-fn mk_vault_dir(tmp_dir: &TempDir) -> Result<()> {
-    debug!("creating vault dir: {}", vault_path(tmp_dir).display());
-    create_dir_all(&vault_path(tmp_dir))?;
-    Ok(())
-}
-
-fn vault_path(tmp_dir: &TempDir) -> PathBuf {
-    tmp_dir.path().join("META-INF/vault")
-}
-
-fn content_path<S: Into<String>>(path: S) -> String {
-    let path = path.into();
-    let parts: Vec<&str> = path.split("jcr_root").collect();
-    assert_eq!(parts.len(), 2);
-    parts[1].into()
-}
-
-fn write_filter_content<S: Into<String>>(tmp_dir: &TempDir, content_path: S) -> Result<()> {
-    let filter_path = format!("{}/filter.xml", vault_path(&tmp_dir).display());
-    let mut filter_file = File::create(&filter_path)?;
-    let filter_content = filter_content(content_path);
-    debug!(
-        "writing content {} to filter {}",
-        filter_content, filter_path
-    );
-    filter_file.write_all(filter_content.as_bytes())?;
-    Ok(())
-}
-
-fn filter_content<S: Into<String>>(path: S) -> String {
-    format!(
-        r#"<?xml version="1.0" encoding="UTF-8"?>
-<workspaceFilter version="1.0">
-    <filter root="{}"/>
-</workspaceFilter>
-        "#,
-        path.into()
-    )
-}
-
-fn write_properties_content(tmp_dir: &TempDir, pkg: &Pkg) -> Result<()> {
-    let prop_path = format!("{}/properties.xml", vault_path(&tmp_dir).display());
-    let mut prop_file = File::create(&prop_path)?;
-    let properties_content = properties_content(&pkg);
-    debug!(
-        "writing content {} to properties file {}",
-        &properties_content, prop_path
-    );
-    prop_file.write_all(properties_content.as_bytes())?;
-    Ok(())
-}
-
-fn properties_content(pkg: &Pkg) -> String {
-    format!(
-        r#"<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
-<properties>
-    <entry key="name">{}</entry>
-    <entry key="version">{}</entry>
-    <entry key="group">{}</entry>
-</properties>"#,
-        pkg.name, pkg.version, pkg.group
-    )
-}
-
 fn zip_pkg(tmp_dir: &TempDir) -> Result<()> {
     let initial_dir = env::current_dir()?;
 
@@ -219,11 +111,11 @@ fn zip_pkg(tmp_dir: &TempDir) -> Result<()> {
     let mut zip = ZipWriter::new(writer);
     let options = FileOptions::default();
 
-    for path in vec!["./jcr_root", "./META-INF"].iter() {
+    for path in &["jcr_root", "./META-INF"] {
         let walkdir = WalkDir::new(path);
         let mut buffer = Vec::new();
         debug!("zipping {}", path);
-        for entry in &mut walkdir.into_iter().flat_map(|e| e.ok()) {
+        for entry in &mut walkdir.into_iter().flat_map(Result::ok) {
             let path = entry.path();
             if path.is_file() {
                 debug!("{} is a file", path.display());
@@ -356,182 +248,9 @@ mod test {
     use super::*;
     use anyhow::Result;
     use std::env;
-    use std::fs::create_dir_all;
     use std::fs::read_to_string;
     use std::path::Path;
     use tempfile::TempDir;
-
-    #[test]
-    fn test_mk_jcr_root_dir() -> Result<()> {
-        // given
-        let tmp_dir = TempDir::new()?;
-
-        // when
-        mk_jcr_root_dir(&tmp_dir)?;
-
-        // then
-        assert_eq!(
-            Path::new(&format!("{}/jcr_root", tmp_dir.path().display())).exists(),
-            true
-        );
-        Ok(())
-    }
-
-    #[test]
-    fn test_mk_vault_dir() -> Result<()> {
-        // given
-        let tmp_dir = TempDir::new()?;
-
-        // when
-        mk_vault_dir(&tmp_dir)?;
-
-        // then
-        assert_eq!(
-            Path::new(&format!("{}/META-INF/vault", tmp_dir.path().display())).exists(),
-            true
-        );
-        Ok(())
-    }
-
-    #[test]
-    fn test_write_filter_content() -> Result<()> {
-        // given
-        let tmp_dir = TempDir::new()?;
-        create_dir_all(&format!("{}/META-INF/vault", tmp_dir.path().display()))?;
-
-        // when
-        write_filter_content(&tmp_dir, "/content/path")?;
-
-        // then
-        assert_eq!(
-            Path::new(&format!(
-                "{}/META-INF/vault/filter.xml",
-                tmp_dir.path().display()
-            ))
-            .exists(),
-            true
-        );
-        let filter_contents = read_to_string(format!(
-            "{}/META-INF/vault/filter.xml",
-            tmp_dir.path().display()
-        ))?;
-        assert_eq!(
-            filter_contents,
-            r#"<?xml version="1.0" encoding="UTF-8"?>
-<workspaceFilter version="1.0">
-    <filter root="/content/path"/>
-</workspaceFilter>
-        "#,
-        );
-        Ok(())
-    }
-
-    #[test]
-    fn test_content_path_with_correct_paths() {
-        // given
-        let path = "/home/zbychu/project/test/jcr_root/content/abc";
-
-        // when
-        let content_path = content_path(path);
-
-        // then
-        assert_eq!(content_path, "/content/abc");
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_content_path_with_broken_paths() {
-        // given
-        let path = "/home/zbychu/project/test/content/abc";
-
-        // should panic
-        content_path(path);
-    }
-
-    #[test]
-    fn test_write_properties_content() -> Result<()> {
-        // given
-        let tmp_dir = TempDir::new()?;
-        create_dir_all(&format!("{}/META-INF/vault", tmp_dir.path().display()))?;
-        let pkg = Pkg::default();
-
-        // when
-        write_properties_content(&tmp_dir, &pkg)?;
-
-        // then
-        assert_eq!(
-            Path::new(&format!(
-                "{}/META-INF/vault/properties.xml",
-                tmp_dir.path().display()
-            ))
-            .exists(),
-            true
-        );
-        let properties_contents = read_to_string(format!(
-            "{}/META-INF/vault/properties.xml",
-            tmp_dir.path().display()
-        ))?;
-        assert_eq!(
-            properties_contents,
-            format!(
-                r#"<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
-<properties>
-    <entry key="name">{}</entry>
-    <entry key="version">{}</entry>
-    <entry key="group">{}</entry>
-</properties>"#,
-                pkg.name, pkg.version, pkg.group
-            ),
-        );
-        Ok(())
-    }
-
-    #[test]
-    fn test_mk_pkg_dir() -> Result<()> {
-        // given
-        let file_path = "/home/user/project/jcr_root/content/client";
-        let pkg = Pkg::default();
-
-        // when
-        let tmp_dir_path = mk_pkg_dir(file_path, &pkg)?;
-
-        // then
-        assert_eq!(
-            Path::new(&format!("{}/jcr_root", tmp_dir_path.path().display())).exists(),
-            true
-        );
-        assert_eq!(
-            Path::new(&format!(
-                "{}/META-INF/vault/filter.xml",
-                tmp_dir_path.path().display()
-            ))
-            .exists(),
-            true
-        );
-        let filter_contents = read_to_string(format!(
-            "{}/META-INF/vault/filter.xml",
-            tmp_dir_path.path().display()
-        ))?;
-        assert_eq!(
-            filter_contents,
-            r#"<?xml version="1.0" encoding="UTF-8"?>
-<workspaceFilter version="1.0">
-    <filter root="/content/client"/>
-</workspaceFilter>
-        "#,
-        );
-        assert_eq!(
-            Path::new(&format!(
-                "{}/META-INF/vault/properties.xml",
-                tmp_dir_path.path().display()
-            ))
-            .exists(),
-            true
-        );
-
-        Ok(())
-    }
 
     #[test]
     fn test_zip_pkg() -> Result<()> {
---
+++ src/pkg.rs
@@ -0,0 +1,298 @@
+use anyhow::Result;
+use log::debug;
+use std::fs::{create_dir_all, File};
+use std::io::prelude::*;
+use std::path::PathBuf;
+use std::time::{SystemTime, UNIX_EPOCH};
+use tempfile::TempDir;
+
+#[derive(Debug)]
+pub(crate) struct Pkg {
+    name: String,
+    version: String,
+    group: String,
+}
+
+impl Pkg {
+    pub(crate) fn path(&self) -> String {
+        format!("{}/{}-{}.zip", self.group, self.name, self.version)
+    }
+}
+
+impl Default for Pkg {
+    fn default() -> Self {
+        Self {
+            name: "je-pkg".into(),
+            version: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("failed to count secs from EPOCH")
+                .as_secs()
+                .to_string(),
+            group: "je".into(),
+        }
+    }
+}
+
+pub(crate) fn mk_pkg_dir(path: &str, pkg: &Pkg) -> Result<TempDir> {
+    debug!("creating pkg dir");
+    let tmp_dir = TempDir::new()?;
+    mk_jcr_root_dir(&tmp_dir)?;
+    mk_vault_dir(&tmp_dir)?;
+    write_filter_content(&tmp_dir, content_path(path))?;
+    write_properties_content(&tmp_dir, pkg)?;
+    Ok(tmp_dir)
+}
+
+fn mk_jcr_root_dir(tmp_dir: &TempDir) -> Result<()> {
+    let jcr_root_dir_path = tmp_dir.path().join("jcr_root");
+    debug!("creating jcr_root dir: {}", jcr_root_dir_path.display());
+    create_dir_all(jcr_root_dir_path)?;
+    Ok(())
+}
+
+fn mk_vault_dir(tmp_dir: &TempDir) -> Result<()> {
+    debug!("creating vault dir: {}", vault_path(tmp_dir).display());
+    create_dir_all(&vault_path(tmp_dir))?;
+    Ok(())
+}
+
+fn vault_path(tmp_dir: &TempDir) -> PathBuf {
+    tmp_dir.path().join("META-INF/vault")
+}
+
+fn content_path<S: Into<String>>(path: S) -> String {
+    let path = path.into();
+    let parts: Vec<&str> = path.split("jcr_root").collect();
+    assert_eq!(parts.len(), 2);
+    parts[1].into()
+}
+
+fn write_filter_content<S: Into<String>>(tmp_dir: &TempDir, content_path: S) -> Result<()> {
+    let filter_path = format!("{}/filter.xml", vault_path(&tmp_dir).display());
+    let mut filter_file = File::create(&filter_path)?;
+    let filter_content = filter_content(content_path);
+    debug!(
+        "writing content {} to filter {}",
+        filter_content, filter_path
+    );
+    filter_file.write_all(filter_content.as_bytes())?;
+    Ok(())
+}
+
+fn filter_content<S: Into<String>>(path: S) -> String {
+    format!(
+        r#"<?xml version="1.0" encoding="UTF-8"?>
+<workspaceFilter version="1.0">
+    <filter root="{}"/>
+</workspaceFilter>
+        "#,
+        path.into()
+    )
+}
+
+fn write_properties_content(tmp_dir: &TempDir, pkg: &Pkg) -> Result<()> {
+    let prop_path = format!("{}/properties.xml", vault_path(&tmp_dir).display());
+    let mut prop_file = File::create(&prop_path)?;
+    let properties_content = properties_content(&pkg);
+    debug!(
+        "writing content {} to properties file {}",
+        &properties_content, prop_path
+    );
+    prop_file.write_all(properties_content.as_bytes())?;
+    Ok(())
+}
+
+fn properties_content(pkg: &Pkg) -> String {
+    format!(
+        r#"<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
+<properties>
+    <entry key="name">{}</entry>
+    <entry key="version">{}</entry>
+    <entry key="group">{}</entry>
+</properties>"#,
+        pkg.name, pkg.version, pkg.group
+    )
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use anyhow::Result;
+    use std::fs::create_dir_all;
+    use std::fs::read_to_string;
+    use std::path::Path;
+    use tempfile::TempDir;
+
+    #[test]
+    fn test_mk_jcr_root_dir() -> Result<()> {
+        // given
+        let tmp_dir = TempDir::new()?;
+
+        // when
+        mk_jcr_root_dir(&tmp_dir)?;
+
+        // then
+        assert_eq!(
+            Path::new(&format!("{}/jcr_root", tmp_dir.path().display())).exists(),
+            true
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_mk_vault_dir() -> Result<()> {
+        // given
+        let tmp_dir = TempDir::new()?;
+
+        // when
+        mk_vault_dir(&tmp_dir)?;
+
+        // then
+        assert_eq!(
+            Path::new(&format!("{}/META-INF/vault", tmp_dir.path().display())).exists(),
+            true
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_filter_content() -> Result<()> {
+        // given
+        let tmp_dir = TempDir::new()?;
+        create_dir_all(&format!("{}/META-INF/vault", tmp_dir.path().display()))?;
+
+        // when
+        write_filter_content(&tmp_dir, "/content/path")?;
+
+        // then
+        assert_eq!(
+            Path::new(&format!(
+                "{}/META-INF/vault/filter.xml",
+                tmp_dir.path().display()
+            ))
+            .exists(),
+            true
+        );
+        let filter_contents = read_to_string(format!(
+            "{}/META-INF/vault/filter.xml",
+            tmp_dir.path().display()
+        ))?;
+        assert_eq!(
+            filter_contents,
+            r#"<?xml version="1.0" encoding="UTF-8"?>
+<workspaceFilter version="1.0">
+    <filter root="/content/path"/>
+</workspaceFilter>
+        "#,
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_content_path_with_correct_paths() {
+        // given
+        let path = "/home/zbychu/project/test/jcr_root/content/abc";
+
+        // when
+        let content_path = content_path(path);
+
+        // then
+        assert_eq!(content_path, "/content/abc");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_content_path_with_broken_paths() {
+        // given
+        let path = "/home/zbychu/project/test/content/abc";
+
+        // should panic
+        content_path(path);
+    }
+
+    #[test]
+    fn test_write_properties_content() -> Result<()> {
+        // given
+        let tmp_dir = TempDir::new()?;
+        create_dir_all(&format!("{}/META-INF/vault", tmp_dir.path().display()))?;
+        let pkg = Pkg::default();
+
+        // when
+        write_properties_content(&tmp_dir, &pkg)?;
+
+        // then
+        assert_eq!(
+            Path::new(&format!(
+                "{}/META-INF/vault/properties.xml",
+                tmp_dir.path().display()
+            ))
+            .exists(),
+            true
+        );
+        let properties_contents = read_to_string(format!(
+            "{}/META-INF/vault/properties.xml",
+            tmp_dir.path().display()
+        ))?;
+        assert_eq!(
+            properties_contents,
+            format!(
+                r#"<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
+<properties>
+    <entry key="name">{}</entry>
+    <entry key="version">{}</entry>
+    <entry key="group">{}</entry>
+</properties>"#,
+                pkg.name, pkg.version, pkg.group
+            ),
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_mk_pkg_dir() -> Result<()> {
+        // given
+        let file_path = "/home/user/project/jcr_root/content/client";
+        let pkg = Pkg::default();
+
+        // when
+        let tmp_dir_path = mk_pkg_dir(file_path, &pkg)?;
+
+        // then
+        assert_eq!(
+            Path::new(&format!("{}/jcr_root", tmp_dir_path.path().display())).exists(),
+            true
+        );
+        assert_eq!(
+            Path::new(&format!(
+                "{}/META-INF/vault/filter.xml",
+                tmp_dir_path.path().display()
+            ))
+            .exists(),
+            true
+        );
+        let filter_contents = read_to_string(format!(
+            "{}/META-INF/vault/filter.xml",
+            tmp_dir_path.path().display()
+        ))?;
+        assert_eq!(
+            filter_contents,
+            r#"<?xml version="1.0" encoding="UTF-8"?>
+<workspaceFilter version="1.0">
+    <filter root="/content/client"/>
+</workspaceFilter>
+        "#,
+        );
+        assert_eq!(
+            Path::new(&format!(
+                "{}/META-INF/vault/properties.xml",
+                tmp_dir_path.path().display()
+            ))
+            .exists(),
+            true
+        );
+
+        Ok(())
+    }
+}
