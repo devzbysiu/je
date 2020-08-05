a5f3e3d7de37779e219e31e390158bdeb5b3f92a
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-08-05 14:13:55 +0200

    Move path tests to path module

--- src/path.rs
+++ src/path.rs
@@ -16,3 +16,30 @@ impl Path {
         self.0.clone()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_content_path_with_correct_paths() {
+        // given
+        let path = Path::new("/home/zbychu/project/test/jcr_root/content/abc");
+
+        // when
+        let content_path = path.content();
+
+        // then
+        assert_eq!(content_path, "/content/abc");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_content_path_with_broken_paths() {
+        // given
+        let path = Path::new("/home/zbychu/project/test/content/abc");
+
+        // should panic
+        path.content();
+    }
+}
--- src/pkgdir.rs
+++ src/pkgdir.rs
@@ -190,28 +190,6 @@ mod test {
     }
 
     #[test]
-    fn test_content_path_with_correct_paths() {
-        // given
-        let path = Path::new("/home/zbychu/project/test/jcr_root/content/abc");
-
-        // when
-        let content_path = path.content();
-
-        // then
-        assert_eq!(content_path, "/content/abc");
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_content_path_with_broken_paths() {
-        // given
-        let path = Path::new("/home/zbychu/project/test/content/abc");
-
-        // should panic
-        path.content();
-    }
-
-    #[test]
     fn test_write_properties_content() -> Result<()> {
         // given
         let tmp_dir = TempDir::new()?;
