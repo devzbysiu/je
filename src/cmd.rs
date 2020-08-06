45d15cb075db0e4ccbbde970c9ef4abad2bdded8
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-08-06 11:24:49 +0200

    Add dst_path fn, fix file upload

--- src/cmd.rs
+++ src/cmd.rs
@@ -9,7 +9,7 @@ use fs_extra::{file, file::CopyOptions as FileOpts};
 use log::{debug, info};
 use std::fs::{self, OpenOptions};
 use std::io::prelude::*;
-use std::path::Path as OsPath;
+use std::path::{Path as OsPath, PathBuf};
 use std::thread;
 use std::time::Duration;
 use structopt::StructOpt;
@@ -107,22 +107,30 @@ pub(crate) fn put(cfg: &Cfg, path: &Path) -> Result<()> {
 }
 
 fn cp_files_to_pkg(path: &Path, tmp_dir: &TempDir) -> Result<()> {
-    let dst_path = tmp_dir.path().join(path.parent_from_root()?);
+    let dst_path = dst_path(path, tmp_dir)?;
     info!(
         "copying files from {} to {}",
         path.full(),
         dst_path.display()
     );
-    fs::create_dir_all(&dst_path)?;
+    fs::create_dir_all(tmp_dir.path().join(path.parent_from_root()?))?;
     if path.is_dir() {
         debug!("{} is a dir", path.full());
         dir::copy(path.full(), dst_path, &DirOpts::new())?;
     } else {
-        let dst_path = tmp_dir.path().join(path.from_root());
         debug!("{} is a file", path.full());
         file::copy(path.full(), dst_path, &FileOpts::new())?;
     }
     Ok(())
+}
+
+fn dst_path(path: &Path, tmp_dir: &TempDir) -> Result<PathBuf> {
+    let result = if path.is_dir() {
+        tmp_dir.path().join(path.parent_from_root()?)
+    } else {
+        tmp_dir.path().join(path.from_root())
+    };
+    Ok(result)
 }
 
 #[cfg(test)]
